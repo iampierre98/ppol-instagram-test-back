74250ffb03d80214334c19340f49e4f0eff7bafc
Carlos <andre021297@gmail.com>
2023-07-19 18:24:36 -0500

    add_comments

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const express = require("express");
 const axios = require("axios");
-const cheerio = require("cheerio");
 const cors = require("cors");
 const morgan = require("morgan");
 const fs = require("fs");
 const app = express();
 const { SocksProxyAgent } = require("socks-proxy-agent");
-const {
-  S3Client,
-  ListBucketsCommand,
-  ListObjectsV2Command,
-  GetObjectCommand,
-  PutObjectCommand,
-} = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
 require("dotenv").config();
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(morgan("dev"));
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
 const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID;
 const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
 const CLOUDFLARE_ID = process.env.CLOUDFLARE_ID;
-const DOWLOAND_URL = process.env.DOWLOAND_URL;
+const DOWNLOAD_URL = process.env.DOWNLOAD_URL;
 const PROXY_URL = process.env.PROXY_URL;
 
+// const CLIENT_ID = process.env.CLIENT_ID;
+// const CLIENT_SECRET = process.env.CLIENT_SECRET;
+// const REDIRECT_URI = process.env.REDIRECT_URI;
+
+//Authentificacion de AWS para usar S3
 const S3 = new S3Client({
   region: "auto",
   endpoint: `https://${CLOUDFLARE_ID}.r2.cloudflarestorage.com`,
@@ -37,13 +33,7 @@ const S3 = new S3Client({
   },
 });
 
-const getProxy = async () => {
-  const response = await axios.get(PROXY_URL);
-  //this will return a txt with the proxies
-  return response.data;
-};
-
-//Axios para guardar los archivos
+//Para crear el nuevo link de descarga de las imagenes
 const getNewUrl = async (url, username, mediaId) => {
   const response = await axios({
     url: url,
@@ -61,10 +51,10 @@ const getNewUrl = async (url, username, mediaId) => {
   });
 
   if (await uploadImage(filePath, `${username + mediaId}.jpg`)) {
-    return `${DOWLOAND_URL}/${username + mediaId}.jpg`;
+    return `${DOWNLOAD_URL}/${username + mediaId}.jpg`;
   }
 };
-
+//Subir las imagenes a nuestro servidor para enviarlas a nuestro almanecenamiento en la nube
 const uploadImage = async (filePath, filename) => {
   const data = fs.readFileSync(filePath);
   const params = {
@@ -90,7 +80,7 @@ app.post("/instagram/", async (req, res) => {
     if (!username) {
       return res.status(400).json({ message: "username is required" });
     }
-    //el usuario puede enviar en link o solo usuario puedes limpiar si en caso viene en link el fromato seria https://www.instagram.com/username/
+    //el usuario puede enviar en link o solo usuario
     let usernameClean = "";
     if (username.includes("/")) {
       usernameClean = username.split(".com/")[1];
@@ -102,16 +92,16 @@ app.post("/instagram/", async (req, res) => {
     } else {
       usernameClean = username;
     }
-    /* initialize proxy configuration */
-    const torProxyAgent = new SocksProxyAgent(PROXY_URL);
+    // Configuracion para usar el proxy tipo SOCKS5
+    const ProxyAgent = new SocksProxyAgent(PROXY_URL);
     const response = await axios.get(
       `https://www.instagram.com/api/v1/users/web_profile_info/?username=${usernameClean}`,
       {
         headers: {
           "X-Ig-App-Id": "936619743392459",
         },
-        httpsAgent: torProxyAgent,
-        htppAgent: torProxyAgent,
+        httpsAgent: ProxyAgent,
+        htppAgent: ProxyAgent,
       }
     );
     const data = response.data.data.user.edge_owner_to_timeline_media.edges;
@@ -123,11 +113,13 @@ app.post("/instagram/", async (req, res) => {
       fbid,
     } = response.data.data.user;
     let profile_url = `https://www.instagram.com/${usernameClean}/`;
+    //Se crea un nuevo link de descarga para la imagen de perfil
     let img_profile_url = await new Promise(async (resolve, reject) => {
       await getNewUrl(profile_pic_url, usernameClean, fbid).then((url) => {
         resolve(url);
       });
     });
+    // Se obtiene la informacion de las imagenes
     const media = data.slice(0, 6).map((item) => {
       const {
         thumbnail_resources,
@@ -144,6 +136,7 @@ app.post("/instagram/", async (req, res) => {
       }
       return { display_url, caption, is_video, mediaId, imgUrl };
     });
+    //Se suben las imagenes a nuestro servidor y se crea un nuevo link de descarga
     const mediaUrl = await Promise.all(
       media.map(async (item) => {
         const display_url = await getNewUrl(
@@ -173,54 +166,34 @@ app.post("/instagram/", async (req, res) => {
     res.status(500).json({ error });
     console.error(error);
   }
-  // try {
-  //   axios
-  //     .get(
-  //       `https://www.instagram.com/api/v1/users/web_profile_info/?username=${username}`,
-  //       {
-  //         headers,
-  //       }
-  //     )
-  //     .then((response) => {
-  //       const data = response.data.data.user.edge_owner_to_timeline_media.edges;
-  //       const images = data.map((item) => {
-  //         const { display_url, shortcode } = item.node;
-  //         return { display_url, shortcode };
-  //       });
-  //     });
-  //   res.status(200).json({ message: "ok" });
-  // } catch (error) {
-  //   console.log(error);
-  //   res.status(500).json({ error });
-  // }
 });
 
-app.post("/instagram/access-token", async (req, res) => {
-  try {
-    console.log(req.body.code);
-    const url = "https://api.instagram.com/oauth/access_token";
-    const formData = new URLSearchParams();
-    formData.append("client_id", CLIENT_ID);
-    formData.append("client_secret", CLIENT_SECRET);
-    formData.append("grant_type", "authorization_code");
-    formData.append("redirect_uri", REDIRECT_URI);
-    formData.append("code", req.body.code);
-    const response = await axios.post(url, formData, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
-    const { access_token } = response.data;
+// app.post("/instagram/access-token", async (req, res) => {
+//   try {
+//     console.log(req.body.code);
+//     const url = "https://api.instagram.com/oauth/access_token";
+//     const formData = new URLSearchParams();
+//     formData.append("client_id", CLIENT_ID);
+//     formData.append("client_secret", CLIENT_SECRET);
+//     formData.append("grant_type", "authorization_code");
+//     formData.append("redirect_uri", REDIRECT_URI);
+//     formData.append("code", req.body.code);
+//     const response = await axios.post(url, formData, {
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//     });
+//     const { access_token } = response.data;
 
-    const largeTokenUrl = `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${CLIENT_SECRET}&access_token=${access_token}`;
-    const largeTokenResponse = await axios.get(largeTokenUrl);
-    const { access_token: largeToken } = largeTokenResponse.data;
-    res.status(200).json({ access_token: largeToken });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error });
-  }
-});
+//     const largeTokenUrl = `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${CLIENT_SECRET}&access_token=${access_token}`;
+//     const largeTokenResponse = await axios.get(largeTokenUrl);
+//     const { access_token: largeToken } = largeTokenResponse.data;
+//     res.status(200).json({ access_token: largeToken });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error });
+//   }
+// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Backend server listening on port 3000");
